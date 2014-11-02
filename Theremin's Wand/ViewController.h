2bb4bdfdfb2b69e776c20cedf66db9fa63807e49
Carla08 <dcfc@live.com.mx>
2014-11-02 16:07:41 -0700

    Initial Commit

---
+++ Theremin's Wand/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Theremin's Wand
+//
+//  Created by Apple on 02/11/14.
+//  Copyright (c) 2014 Carla & Mario. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Theremin's Wand/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Theremin's Wand
+//
+//  Created by Apple on 02/11/14.
+//  Copyright (c) 2014 Carla & Mario. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
