8f91cd9f0c1f9ae691f43ad1993fef260426d6a9
Carla08 <dcfc@live.com.mx>
2014-11-17 23:53:30 -0700

    Notes with acceleration in x

--- Theremin's Wand/ViewController.h
+++ Theremin's Wand/ViewController.h
@@ -7,10 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
+#import <AVFoundation/AVFoundation.h>
 #import "RecordedVC.h"
+
+
 @interface ViewController : UIViewController
 {
-
+    __weak IBOutlet UILabel *x_acc;
+    
+    __weak IBOutlet UILabel *y_acc;
+    
+    __weak IBOutlet UILabel *z_acc;
+    
+    __weak IBOutlet UILabel *Note;
+    BOOL play_pressed;
 }
+- (IBAction)Theremin_play:(id)sender;
+- (IBAction)Theremin_stop:(id)sender;
+-(NSString *)getNote: (NSInteger) x_acc;
+
+@property (strong,nonatomic) CMMotionManager *manager;
+@property (strong,nonatomic) AVAudioPlayer *player;
+
 @end
 
--- Theremin's Wand/ViewController.m
+++ Theremin's Wand/ViewController.m
@@ -17,18 +17,90 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+   
+    self.manager = [[CMMotionManager alloc] init];
+    [NSTimer scheduledTimerWithTimeInterval:0.2 target:self selector:@selector(getValues:) userInfo:nil repeats:YES];
+    self.manager.accelerometerUpdateInterval = 0.1;  // 40 Hz
+    [self.manager startAccelerometerUpdates];
+    
 }
+
+
+-(void) getValues:(NSTimer *) timer {
+    NSInteger x_note= ABS(self.manager.accelerometerData.acceleration.x*100);
+    x_acc.text = [NSString stringWithFormat:@"%.2f",self.manager.accelerometerData.acceleration.x];//NOTAS
+    y_acc.text = [NSString stringWithFormat:@"%.2f",self.manager.accelerometerData.acceleration.y];//CONSTANTE
+    z_acc.text = [NSString stringWithFormat:@"%.2f",self.manager.accelerometerData.acceleration.z];
+    Note.text=[self getNote:x_note];
+    if (self.player.playing==false) {
+        NSURL *note_path = [NSURL fileURLWithPath:[[NSBundle mainBundle]
+                                                   pathForResource:[self getNote:x_note]
+                                                   ofType:@"mp3"]];
+        self.player=[[AVAudioPlayer alloc] initWithContentsOfURL:note_path error:nil];
+        
+    }
+    
+    if(play_pressed==true){
+        [self.player play];
+    }
+}
+-(NSString *) getNote: (NSInteger) x{
+    if((x>=0)&&(x<8)){
+        return @"C_middle";
+    }
+    if((x>=8)&&(x<16)){
+        return @"C_sharp";
+    }
+    if((x>=16)&&(x<24)){
+        return @"D";
+    }
+    if((x>=24)&&(x<32)){
+        return @"D_sharp";
+    }
+    if((x>=32)&&(x<40)){
+        return @"E";
+    }
+    if((x>=40)&&(x<48)){
+        return @"F";
+    }
+    if((x>=48)&&(x<56)){
+        return @"F_sharp";
+    }
+    if((x>=56)&&(x<64)){
+        return @"G";
+    }
+    if((x>=64)&&(x<72)){
+        return @"G_sharp";
+    }
+    if((x>=72)&&(x<80)){
+        return @"A";
+    }
+    if((x>=80)&&(x<88)){
+        return @"A_sharp";
+    }
+    if((x>=88)&&(x<96)){
+        return @"B";
+    }
+    if((x>=96)&&(x<104)){
+        return @"B";
+    }
+    return @"";
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)Theremin_play:(id)sender {
+    play_pressed=true;
+    
+}
 
-
-/*- (IBAction)Recorded:(id)sender {
-    NSLog(@"Yeah babe");
-    RecordedVC *RVC= [[RecordedVC alloc]init];
-    [self presentViewController:RVC animated:YES completion:nil];
-}*/
+- (IBAction)Theremin_stop:(id)sender {
+    play_pressed=false;
+    
+}
 @end
